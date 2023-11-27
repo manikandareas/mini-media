f38ebafa1521b78fc078eea68514a28b7e7b3589
manikandareas <vitoandareas15@gmail.com>
2023-11-27 15:42:23 +0800

    ui: adding loading ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ import FeedAside from "./_components/feed/feed-aside";
 export default function Home() {
   return (
     <>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <Feed />
-      </Suspense>
+      <Feed />
       <FeedAside />
     </>
   );
