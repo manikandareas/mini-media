8342aa8b57b9434e7913592ef6fdfa78c5bbb400
manikandareas <vitoandareas15@gmail.com>
2023-11-27 15:29:38 +0800

    fix: fixing fetch data ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import Feed from "./_components/feed/feed";
 import FeedAside from "./_components/feed/feed-aside";
 
 export default function Home() {
   return (
     <>
-      <Feed />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Feed />
+      </Suspense>
       <FeedAside />
     </>
   );
