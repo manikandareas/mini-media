e0af66491bc5cfda764793cfea63bbb9d4df70af
manikandareas <vitoandareas15@gmail.com>
2023-11-27 15:11:19 +0800

    fix: fixing fetch data ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import { api } from "~/trpc/server";
 import Feed from "./_components/feed/feed";
 import FeedAside from "./_components/feed/feed-aside";
 import Post from "./_components/feed/post";
+import type { RouterOutputs } from "~/trpc/shared";
+
+type PostsResponse = RouterOutputs["post"]["getAll"];
 
 export default async function Home() {
-  const data = await api.post.getAll.query();
+  const data: PostsResponse = await api.post.getAll.query();
+
   return (
     <>
       <Feed>
-        {data ? (
+        {data.length !== 0 ? (
           data.map((post) => (
             <Post
               key={post.post.id}
